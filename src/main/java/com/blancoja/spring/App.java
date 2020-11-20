143a6b67eea5599f5de75b6fd5f0f18d7bb3d349
JavitxuWeiss <javitxuweiss@gmail.com>
2020-11-20 19:46:20 +0100

    second practice

--- src/main/java/com/blancoja/spring/App.java
+++ src/main/java/com/blancoja/spring/App.java
@@ -2,29 +2,32 @@ package com.blancoja.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.blancoja.beans.AppConfig2;
-import com.blancoja.beans.Appconfig;
-import com.blancoja.beans.Mundo;
+//import com.blancoja.beans.AppConfig2;
+//import com.blancoja.beans.Appconfig;
+//import com.blancoja.beans.Mundo;
+import com.blancoja.beans.Persona;
 
 public class App {
 	
 	public static void main(String[] args) {
 		
-		//ApplicationContext appcontext= new ClassPathXmlApplicationContext("com/blancoja/xml/beans.xml");
-		AnnotationConfigApplicationContext appcontext= new AnnotationConfigApplicationContext();
-		appcontext.register(Appconfig.class);
-		appcontext.register(AppConfig2.class);
-		appcontext.refresh();
-		
+		ApplicationContext appcontext= new ClassPathXmlApplicationContext("com/blancoja/xml/beans.xml");
+		Persona per= (Persona) appcontext.getBean("persona");
+		/*
+		 * AnnotationConfigApplicationContext appcontext= new
+		 * AnnotationConfigApplicationContext(Appconfig.class,AppConfig2.class);
+		 * appcontext.register(Appconfig.class); appcontext.register(AppConfig2.class);
+		 * appcontext.refresh();
+		 */
 		//Mundo m = appcontext.getBean(Mundo.class); //dos maneras de acceder a un bean desde xml definition
-		Mundo m2 = (Mundo) appcontext.getBean("marte");
+		//Mundo m2 = (Mundo) appcontext.getBean("marte");
 		
 
 		
-		System.out.println(m2.getSaludo());
+		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo());
 		
 		((ConfigurableApplicationContext)appcontext).close();
 		
