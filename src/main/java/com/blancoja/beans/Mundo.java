7cab89acd07ef16af60a73bd8690f22f14401019
JavitxuWeiss <javitxuweiss@gmail.com>
2020-11-20 19:26:51 +0100

    second practice

--- src/main/java/com/blancoja/beans/Mundo.java
+++ src/main/java/com/blancoja/beans/Mundo.java
@@ -1,7 +1,10 @@
 package com.blancoja.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Mundo {
 	
+	@Value("Hola petardillo")
 	private String saludo;
 
 	public String getSaludo() {
--- src/main/java/com/blancoja/spring/App.java
+++ src/main/java/com/blancoja/spring/App.java
@@ -2,19 +2,27 @@ package com.blancoja.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.blancoja.beans.AppConfig2;
+import com.blancoja.beans.Appconfig;
 import com.blancoja.beans.Mundo;
 
 public class App {
 	
 	public static void main(String[] args) {
 		
-		ApplicationContext appcontext= new ClassPathXmlApplicationContext("com/blancoja/xml/beans.xml");
-		Mundo m = appcontext.getBean(Mundo.class); //dos maneras de acceder a un bean desde xml definition
-		Mundo m2 = (Mundo) appcontext.getBean("mundo");
+		//ApplicationContext appcontext= new ClassPathXmlApplicationContext("com/blancoja/xml/beans.xml");
+		AnnotationConfigApplicationContext appcontext= new AnnotationConfigApplicationContext();
+		appcontext.register(Appconfig.class);
+		appcontext.register(AppConfig2.class);
+		appcontext.refresh();
 		
-		System.out.println(m.getSaludo());
+		//Mundo m = appcontext.getBean(Mundo.class); //dos maneras de acceder a un bean desde xml definition
+		Mundo m2 = (Mundo) appcontext.getBean("marte");
+		
+
 		
 		System.out.println(m2.getSaludo());
 		
